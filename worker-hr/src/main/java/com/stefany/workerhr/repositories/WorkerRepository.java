0961602095da5b451b5950564314e999eee6f4ad
Stefany-Silveira <stefany.silveira21@gmail.com>
2022-09-26 18:29:37 -0300

    refactor project hr-worker

--- worker-hr/src/main/java/com/stefany/workerhr/repositories/WorkerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stefany.workerhr.repositories;
-
-import com.stefany.workerhr.entities.Worker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long> {
-}
