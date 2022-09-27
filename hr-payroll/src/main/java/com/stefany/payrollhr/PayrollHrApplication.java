42502b3b52636b39b427ba30270914ae52693b4d
Stefany-Silveira <stefany.silveira21@gmail.com>
2022-09-26 22:07:25 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/main/java/com/stefany/payrollhr/PayrollHrApplication.java
@@ -0,0 +1,13 @@
+package com.stefany.payrollhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollHrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollHrApplication.class, args);
+	}
+
+}
---
+++ hr-payroll/src/test/java/com/stefany/payrollhr/PayrollHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefany.payrollhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollHrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
