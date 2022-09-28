75d4ab6b3252e230478f307ed9341fec473b4261
Stefany-Silveira <stefany.silveira21@gmail.com>
2022-09-28 15:34:51 -0300

    UserRepository, userResource, Zuul config

---
+++ hr-user/src/main/java/com/stefany/hruser/config/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.stefany.hruser.config.repositories;
+
+import com.stefany.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
